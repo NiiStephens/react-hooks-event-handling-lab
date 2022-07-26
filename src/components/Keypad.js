711b25d2895d0eee567bf255c8abedb1ee0403ab
Nii <bnstephens001@gmail.com>
2022-07-26 14:46:14 +0000

    Completed Assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function keypadEvent (){
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <div>
+           <input type = "password"  onChange={keypadEvent} placeholder = "Type Password" />
+        </div>
     )
 }
 
